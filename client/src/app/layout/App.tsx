f92726ee905a95e3e0ecf03cee29e676d8fffa14
sjeren1501 <groovepromo@gmail.com>
2024-01-22 20:41:53 +0100

    added material ui and images

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Product } from "../models/product";
 import Catalog from "../../features/catalog/Catalog";
+import { Typography } from "../../../node_modules/@mui/material/index";
 
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
@@ -27,7 +28,7 @@ function App() {
 
   return (
     <div>
-      <h1>Re-Store</h1>
+      <Typography variant="h1">Re-Store</Typography>
       <Catalog products={products} addProduct={addProduct} />
     </div>
   );
