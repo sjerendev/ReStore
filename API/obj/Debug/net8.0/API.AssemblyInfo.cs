7613c0926b477d2d71c0da23174ba65b3649bc1f
patrik.sjeren <patrik.sjeren@svenskcater.se>
2024-01-25 11:26:25 +0100

    added product details

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c4fe3df8fbcf668955aaef15aa59a90d620f8a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+115c0331c2b36be8f8108304d196d4f7ca4e6a03")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
