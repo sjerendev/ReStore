3a4bd90d8f305796c2165f32aa0851a9ce1acb94
patrik.sjeren <patrik.sjeren@svenskcater.se>
2024-01-30 15:28:31 +0100

    changed about page wirh errors

--- client/src/features/about/AboutPage.tsx
+++ client/src/features/about/AboutPage.tsx
@@ -10,31 +10,43 @@ export default function AboutPage() {
       <ButtonGroup fullWidth>
         <Button
           variant="contained"
-          onClick={() => agent.TestErrors.get400Error()}
+          onClick={() =>
+            agent.TestErrors.get400Error().catch((error) => console.log(error))
+          }
         >
           Test 400 error
         </Button>
         <Button
           variant="contained"
-          onClick={() => agent.TestErrors.get401Error()}
+          onClick={() =>
+            agent.TestErrors.get401Error().catch((error) => console.log(error))
+          }
         >
           Test 401 error
         </Button>
         <Button
           variant="contained"
-          onClick={() => agent.TestErrors.get404Error().catch(error => console.log(error))}
+          onClick={() =>
+            agent.TestErrors.get404Error().catch((error) => console.log(error))
+          }
         >
           Test 404 error
         </Button>
         <Button
           variant="contained"
-          onClick={() => agent.TestErrors.get500Error()}
+          onClick={() =>
+            agent.TestErrors.get500Error().catch((error) => console.log(error))
+          }
         >
           Test 500 error
         </Button>
         <Button
           variant="contained"
-          onClick={() => agent.TestErrors.getValidationError()}
+          onClick={() =>
+            agent.TestErrors.getValidationError().catch((error) =>
+              console.log(error)
+            )
+          }
         >
           Test validation error
         </Button>
